737c94f1da143efa93a0059cf90c4bc0e98549a7
Víctor Ávila <victoraavila2000@gmail.com>
2023-12-07 22:00:14 -0300

    Initial Commit

---
+++ myGuessTheFlag/myGuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  myGuessTheFlagApp.swift
+//  myGuessTheFlag
+//
+//  Created by Víctor Ávila on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct myGuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
